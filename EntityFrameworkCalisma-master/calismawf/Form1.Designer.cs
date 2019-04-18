81f8ca2e910749c34795ee2ec4782e5318fafa3e
mustafa <mustafaisk6@gmail.com>
2019-04-18 10:19:34 +0300

    SON

---
+++ EntityFrameworkCalisma-master/calismawf/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace calismawf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.btnUrunGetir = new System.Windows.Forms.Button();
+            this.btnSipMus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(740, 205);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(44, 259);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(75, 23);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Sipariş Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.BtnGetir_Click);
+            // 
+            // btnUrunGetir
+            // 
+            this.btnUrunGetir.Location = new System.Drawing.Point(141, 259);
+            this.btnUrunGetir.Name = "btnUrunGetir";
+            this.btnUrunGetir.Size = new System.Drawing.Size(75, 23);
+            this.btnUrunGetir.TabIndex = 2;
+            this.btnUrunGetir.Text = "Ürün getir";
+            this.btnUrunGetir.UseVisualStyleBackColor = true;
+            this.btnUrunGetir.Click += new System.EventHandler(this.BtnUrunGetir_Click);
+            // 
+            // btnSipMus
+            // 
+            this.btnSipMus.Location = new System.Drawing.Point(241, 259);
+            this.btnSipMus.Name = "btnSipMus";
+            this.btnSipMus.Size = new System.Drawing.Size(75, 23);
+            this.btnSipMus.TabIndex = 3;
+            this.btnSipMus.Text = "Sip.Çal.";
+            this.btnSipMus.UseVisualStyleBackColor = true;
+            this.btnSipMus.Click += new System.EventHandler(this.BtnSipMus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSipMus);
+            this.Controls.Add(this.btnUrunGetir);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Button btnUrunGetir;
+        private System.Windows.Forms.Button btnSipMus;
+    }
+}
+
---
+++ EntityFrameworkCalisma-master/calismawf/Form1.cs
@@ -0,0 +1,70 @@
+﻿using calismawf.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calismawf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TSQLDATAEntities db = new TSQLDATAEntities();
+
+        private void BtnGetir_Click(object sender, EventArgs e)
+        {
+            /* Orders tablosundan fiyatı 500 ve 800 arasında olan siparişleri getir  ve ülkelerine göre sırala*/
+            dataGridView1.DataSource = db.Orders.Where(or=> or.freight>500 && or.freight<800).OrderBy(o=> o.shipcountry).Select(x=> new
+            {
+                x.shipcountry,
+                x.shipcity,
+                x.custid,
+                x.empid,
+                x.freight
+            }
+                ).ToList();
+        }
+
+        private void BtnUrunGetir_Click(object sender, EventArgs e)
+        {
+            /*.fiyatı 20'den büyük ve içinde A harfi içeren ürünlerin id'sini adını ve fiyatını getirme*/
+            dataGridView1.DataSource = db.Products.Where(p=>p.unitprice>20 && p.productname.Contains("A")).Select(x => new
+            {
+                x.productid,
+                x.productname,
+                x.unitprice
+            }).ToList();
+        }
+
+        private void BtnSipMus_Click(object sender, EventArgs e)
+        {
+            /* Siparişler ve çalışanlar tablosundan veri çekme ve ülke ve şehirlere göre sıralama*/
+            dataGridView1.DataSource = (from o in db.Orders
+                                        join em in db.Employees on o.empid equals em.empid
+                                        where o.empid == em.empid
+                                        select new
+                                        {
+                                            o.empid,
+                                            o.custid,
+                                            o.Customer,
+                                            o.shipcountry,
+                                            o.shipcity,
+                                            o.shipname,
+                                            em.firstname,
+                                            em.lastname,
+                                            em.region
+                                        }).OrderBy(x=> x.shipcountry).OrderBy(y=>y.shipcity).ToList();
+
+
+        }
+    }
+}
